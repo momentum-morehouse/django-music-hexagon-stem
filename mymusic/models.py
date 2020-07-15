29cfd4518efb41a73993cc1d896b305eb31b645f
hexagonstem <bishara@hexagon-stem.com>
2020-07-15 01:30:38 +0000

    Add homepage styles.css

--- mymusic/models.py
+++ mymusic/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 
 class Album(models.Model):
-  artist_name = models.CharField(max_lenth=255)
+  artist_name = models.CharField(max_length=255)
   title = models.CharField(max_length=255)
   date_released = models.DateField(null=True, blank=True)
 
--- mymusic/views.py
+++ mymusic/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Album, Users
+from .models import Album
 
 # Create your views here.
 def list_albums(request):
   albums = Album.objects.all()
-  return render(request, 'albums/list_albums.html', context={'albums':albums})
+  return render(request, 'mymusic/list_albums.html', context={'albums':albums})
